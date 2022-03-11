af2efb432b227f4c92f4f73ee476124a907adf77
MassimoSun <43518047+MassimoSun@users.noreply.github.com>
2022-03-11 23:16:13 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+echo "# retfgg" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/MassimoSun/retfgg.git
+git push -u origin master
+…or push an existing repository from the command line
+git remote add origin https://github.com/MassimoSun/retfgg.git
+git branch -M master
+git push -u origin master
